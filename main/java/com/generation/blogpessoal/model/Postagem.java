811467058491dfc22db6d9a1b298299c7c4f8c83
Rafassz <rafassz.aps@gmail.com>
2024-11-28 15:41:26 -0300

    Blog Pessoalv0.05

--- main/java/com/generation/blogpessoal/model/Postagem.java
+++ main/java/com/generation/blogpessoal/model/Postagem.java
@@ -14,46 +14,53 @@ import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "tb_postagens")
-public class Postagens {
+public class Postagem {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;	
-	
+	private Long id;
+
 	@NotBlank
-	@Size(min = 5, max = 100 )
+	@Size(min = 5, max = 100)
 	private String titulo;
-	
+
 	@NotBlank
-	@Size(min = 5, max = 1000 )
+	@Size(min = 5, max = 1000)
 	private String texto;
-	
+
 	@UpdateTimestamp
 	private LocalDateTime data;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getTexto() {
 		return texto;
 	}
+
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
+
 	public LocalDateTime getData() {
 		return data;
 	}
+
 	public void setData(LocalDateTime data) {
 		this.data = data;
 	}
-	
+
 }
